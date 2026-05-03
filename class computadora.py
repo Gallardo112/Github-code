0ad44e44352a70728674800397fadd3e005e28a4
Gallardo112 <saulgallardov@uanl.edu.mx>
2026-05-03 15:03:20 -0600

    Creación de la clase Computadora

---
+++ class computadora.py
@@ -0,0 +1,61 @@
+class computadora:
+    def __init__(self, id, marca, modelo, procesador, ram):
+        self.__id = id
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__procesador = procesador
+        self.__ram = ram
+    # Getters
+    def get_id(self):
+        return self.__id
+
+    def get_marca(self):
+        return self.__marca
+
+    def get_modelo(self):
+        return self.__modelo
+
+    def get_procesador(self):
+        return self.__procesador
+
+    def get_ram(self):
+        return self.__ram
+    # Setters
+    def set_id(self, id):
+        self.__id = id
+
+    def set_marca(self, marca):
+        self.__marca = marca
+
+    def set_modelo(self, modelo):
+        self.__modelo = modelo
+
+    def set_procesador(self, procesador):
+        self.__procesador = procesador
+
+    def set_ram(self, ram):
+        if ram > 0:
+            self.__ram = ram
+    #metodo 1 aumentar ram
+    def aumentar_ram(self, cantidad):
+        if cantidad > 0:
+            self.__ram += cantidad
+    #metodo 2 metodo de rendimeinto
+    def rendimiento(self):
+        if self.__procesador == "i7" and self.__ram >= 16:
+            return "Alto rendimiento"
+        elif self.__procesador == "i5" and self.__ram >= 8:
+            return "Rendimiento medio"
+        else:
+            return "Rendimiento bajo"  
+    #metodo 3 mostrar informacion
+    def mostrar_informacion(self):
+        return f"ID: {self.__id}, Marca: {self.__marca}, Modelo: {self.__modelo}, Procesador: {self.__procesador}, RAM: {self.__ram}GB"
+
+    # PRUEBA DE LA CLASE
+compu1 = computadora(1, "Dell", "XPS 15", "i7", 16)
+print(compu1.mostrar_informacion())
+print(compu1.rendimiento())
+compu1.aumentar_ram(8)
+print(compu1.mostrar_informacion())
+    
