d9ade29fa44d3a0c4e028caa72eab52e9bf3691b
Gallardo112 <saulgallardov@uanl.edu.mx>
2026-05-03 15:05:01 -0600

    Agregar getters

--- class computadora.py
+++ class computadora.py
@@ -4,7 +4,22 @@ class computadora:
         self.__modelo = modelo
         self.__procesador = procesador
         self.__ram = ram
+#getters
+    def get_id(self):
+        return self.__id
+    
+    def get_marca(self):
+        return self.__marca
+    
+    def get_modelo(self):
+        return self.__modelo
+    
+    def get_procesador(self):
+        return self.__procesador
 
+    def get_ram(self):
+        return self.__ram
+    
     
 
    
